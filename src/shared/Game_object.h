c0cdf6f2b706abc3ed11521938a947b78c6794aa
slawtul <slawtul@javablues.org>
2020-05-20 21:58:19 +0200

    Refactoring...

--- src/shared/Game_object.h
+++ src/shared/Game_object.h
@@ -6,25 +6,9 @@
 #include "Input_component.h"
 #include "Graphics_component.h"
 #include "Physics_component.h"
-#include "Object_name.h"
+#include "Game_object_motion.h"
+#include "Game_object_meta.h"
 
-struct Meta
-{
-    Object_name name{Object_name::NONE};
-
-    // You can do physics and graphics calculations on 'active' object only
-    // If game object is not active - it's removed from game objects vector
-    bool is_active{true};
-};
-
-struct Motion
-{
-    double max_speed{0.0};
-    double angle{0.0};
-    double dx{0.0};
-    double dy{0.0};
-    double acceleration{0.2};
-};
 
 struct Game_object
 {
@@ -32,8 +16,8 @@ struct Game_object
     std::unique_ptr<Physics_component> physics{nullptr};
     std::unique_ptr<Graphics_component> graphics{nullptr};
 
-    Meta meta{};
-    Motion motion{};
+    Game_object_meta meta{};
+    Game_object_motion motion{};
     SDL_Rect rect{};
 
     Game_object(std::unique_ptr<Input_component> input_,
---
+++ src/shared/Game_object_motion.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Game_object_motion
+{
+    double max_speed{0.0};
+    double angle{0.0};
+    double dx{0.0};
+    double dy{0.0};
+    double acceleration{0.2};
+};
