8f456ee91ea0e586a16f0c1f73f743a8f0e3ebc7
slawtul <slawtul@javablues.org>
2020-05-20 21:47:45 +0200

    Remove useless lines

--- src/bullets/Bullet_graphics_component.h
+++ src/bullets/Bullet_graphics_component.h
@@ -14,12 +14,6 @@ struct Bullet_graphics_component: Graphics_component
             return;
         }
 
-        auto[x, y] = calc_position_if_edge_screen_reached(
-            obj.rect.x, obj.rect.y, obj.rect.w, obj.rect.h, graphics
-        );
-        obj.rect.x = x;
-        obj.rect.y = y;
-
         SDL_RenderCopyEx(
             graphics.renderer, graphics.texture_shelf.get_texture("bullet_short_single"),
             nullptr, &obj.rect, obj.motion.angle + 90, nullptr, SDL_FLIP_NONE
