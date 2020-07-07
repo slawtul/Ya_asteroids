934ece7125542a3f854baed0190370240ddd89c4
slawtul <slawtul@javablues.org>
2020-07-07 13:58:42 +0200

    Add more cpp files

--- src/debug_info/Objects_debug_info.h
+++ src/debug_info/Objects_debug_info.h
@@ -10,42 +10,15 @@ struct Objects_debug_info
     uint16_t objs_on_screen{0};
     uint16_t max_objs_on_screen{0};
 
-    uint16_t count_objs()
-    {
-        if (objs_on_screen > max_objs_on_screen)
-        {
-            max_objs_on_screen = objs_on_screen;
-        }
-        return max_objs_on_screen;
-    }
+    uint16_t count_objs();
 
     void render_obj_qty(
         TTF_Font *font,
         SDL_Color color,
-        SDL_Renderer *renderer) const
-    {
-        auto rect = SDL_Rect{20, 56, 120, 16};
-        Render_text rt;
-        rt.render(
-            rect,
-            font,
-            color,
-            renderer,
-            "Obj_qty: " + std::to_string(objs_on_screen));
-    }
+        SDL_Renderer *renderer) const;
 
     void render_max_obj_qty(
         TTF_Font *font,
         SDL_Color color,
-        SDL_Renderer *renderer)
-    {
-        auto rect = SDL_Rect{20, 72, 120, 16};
-        Render_text rt;
-        rt.render(
-            rect,
-            font,
-            color,
-            renderer,
-            "Max_qty: " + std::to_string(count_objs()));
-    }
+        SDL_Renderer *renderer);
 };
