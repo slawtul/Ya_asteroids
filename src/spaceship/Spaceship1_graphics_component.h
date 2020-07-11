c8db878684305b3609189df860238eacd34e453b
slawtul <slawtul@javablues.org>
2020-07-11 21:28:44 +0200

    Refactoring...

--- src/spaceship/Spaceship1_graphics_component.h
+++ src/spaceship/Spaceship1_graphics_component.h
@@ -1,33 +1,11 @@
-#pragma once
+#ifndef SPACESHIP1_GRAPHICS_COMPONENT_H
+#define SPACESHIP1_GRAPHICS_COMPONENT_H
 
 #include "Game_object.h"
 
 struct Spaceship1_graphics_component: Graphics_component
 {
-    void update(Game_object &obj, Graphics &graphics) override
-    {
-        obj.rect.x += static_cast<int>(obj.motion.dx);
-        obj.rect.y += static_cast<int>(obj.motion.dy);
-
-        auto[x, y] = calc_position_if_edge_screen_reached(
-            obj.rect.x,
-            obj.rect.y,
-            obj.rect.w,
-            obj.rect.h,
-            graphics
-        );
-
-        obj.rect.x = x;
-        obj.rect.y = y;
-
-        SDL_RenderCopyEx(
-            graphics.renderer,
-            graphics.texture_shelf.get_texture("WO84-wu-X1"),
-            nullptr,
-            &obj.rect,
-            obj.motion.angle + 90,
-            nullptr,
-            SDL_FLIP_NONE
-        );
-    }
+    void update(Game_object &obj, Graphics &graphics) override;
 };
+
+#endif //SPACESHIP1_GRAPHICS_COMPONENT_H
--- src/spaceship/Spaceship1_input_component.h
+++ src/spaceship/Spaceship1_input_component.h
@@ -1,28 +1,11 @@
-#pragma once
+#ifndef SPACESHIP1_INPUT_COMPONENT_H
+#define SPACESHIP1_INPUT_COMPONENT_H
 
 #include "Game_object.h"
 
 struct Spaceship1_input_component: Input_component
 {
-    void update(Game_object &obj) override
-    {
-        if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_RIGHT])
-        {
-            obj.motion.angle += 4.0;
-        }
-        if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_LEFT])
-        {
-            obj.motion.angle -= 4.0;
-        }
-        if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_UP])
-        {
-            obj.motion.acceleration = 0.6;
-            obj.motion.max_speed = 8.0;
-        }
-        else
-        {
-            obj.motion.acceleration = 0.2;
-            obj.motion.max_speed = 6.0;
-        }
-    }
+    void update(Game_object &obj) override;
 };
+
+#endif //SPACESHIP1_INPUT_COMPONENT_H
