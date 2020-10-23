42d08ebed18b0876444ffe94025c6f1328cf74c5
slawtul <slawtul@javablues.org>
2020-10-23 19:26:14 +0200

    Clean up

--- src/start_game.cpp
+++ src/start_game.cpp
@@ -20,13 +20,10 @@ int main(int argc, char *argv[])
   TTF_Init();
 
   const auto window = sdl2.create_window(
-    "YA Asteroids", 1920, 1080,
-    SDL_WINDOW_RESIZABLE |
-      SDL_WINDOW_ALLOW_HIGHDPI);
+    "YA Asteroids", 1920, 1080, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
   const auto renderer = sdl2.create_renderer(
-    window,
-    SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    window, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
   Graphics_debug_info gdi;
   gdi.log_screen_size(renderer);
