56967199bb7b487a5e0984f5510b717304b09ee6
slawtul <slawtul@javablues.org>
2020-11-05 13:10:27 +0100

    Clean up

--- src/shared/gfx_helpers.cpp
+++ src/shared/gfx_helpers.cpp
@@ -9,10 +9,7 @@ bool gfx_helpers::is_off_screen(int rect_x, int rect_y, SDL_Renderer* renderer)
     return (rect_x < 0 || rect_y < 0) || (rect_x > screen_width || rect_y > screen_height);
 }
 
-std::pair<int, int> gfx_helpers::opposite_edge_position(int rect_x,
-        int rect_y,
-        int rect_w,
-        int rect_h,
+std::pair<int, int> gfx_helpers::opposite_edge_position(int rect_x, int rect_y, int rect_w, int rect_h,
         SDL_Renderer* renderer)
 {
     int screen_width{}, screen_height{};
